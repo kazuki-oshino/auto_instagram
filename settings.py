4a2287f7f65d12c8221c267f42a8fdf2688d6c5d
kazuikios <slash0juice@gmail.com>
2021-08-11 15:55:34 +0900

    create auto like processing

--- settings.py
+++ settings.py
@@ -8,3 +8,6 @@ load_dotenv(dotenv_path)
 USERNAME = os.environ.get("USERNAME")
 PASSWORD = os.environ.get("PASSWORD")
 LOG_DIR = os.environ.get("LOG_DIR")
+
+# TARGET_TAG_DICT = {'御朱印': 5, 'スイーツ女子': 5, '40代ママ': 5, '50代ライフスタイル': 5}
+TARGET_TAG_DICT = {'御朱印': 2}
