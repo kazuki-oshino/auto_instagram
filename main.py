c95002df9851698c74186c54f0b7ebfd69889d67
kazuikios <slash0juice@gmail.com>
2021-08-19 13:41:52 +0900

    move logout process to auto_like's inner processing

--- main.py
+++ main.py
@@ -23,15 +23,13 @@ def main():
         # 自動いいね処理
         auto_like = instagram.instagram_auto_like.InstagramAutoLike(driver_util.get_driver(), logger)
         auto_like.auto_like(csv_util.make_tag_dict())
+        # ログアウト
+        instagram_common.log_out_instagram()
     else:
         # フォロワー洗い出し処理
         follower = instagram.instagram_follower.InstagramFollower(driver_util.get_driver(), logger)
         follower.access_my_page()
         test = follower.get_follower()
-
-    # ログアウト
-    # TODO 実施した機能ごとに処理が変わりそう
-    instagram_common.log_out_instagram()
 
 
 if __name__ == '__main__':
