4f72ecd8b2ccf28a7846eaf297277b4b0c8df409
kazuikios <slash0juice@gmail.com>
2021-08-19 13:39:44 +0900

    changed content path to absolute path

--- csv_util.py
+++ csv_util.py
@@ -3,7 +3,7 @@ import csv
 
 def make_tag_dict() -> dict:
     res = {}
-    with open('tag/auto_like_tag.csv') as f:
+    with open('/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag.csv') as f:
         reader = csv.reader(f)
         for row in reader:
             res[str(row[0]).strip()] = int(str(row[1]).strip())
