15434c5360f317bf89fb85aeebec40a2be2778c1
kazuikios <slash0juice@gmail.com>
2021-08-23 14:46:46 +0900

    change to select target csv file

--- csv_util.py
+++ csv_util.py
@@ -1,9 +1,23 @@
 import csv
+import sys
+
+csv_path_dict = {
+    1: '/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag1.csv',
+    2: '/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag2.csv',
+    3: '/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag3.csv',
+    4: '/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag4.csv'
+}
 
 
 def make_tag_dict() -> dict:
+    # 対象CSVを起動時引数から取得
+    target_csv_key: int = 1
+    args = sys.argv
+    if len(args) > 1:
+        target_csv_key = int(args[1])
+
     res = {}
-    with open('/Users/kazuki/PycharmProjects/auto_instagram/tag/auto_like_tag.csv') as f:
+    with open(csv_path_dict[target_csv_key]) as f:
         reader = csv.reader(f)
         for row in reader:
             res[str(row[0]).strip()] = int(str(row[1]).strip())
